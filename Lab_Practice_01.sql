74d5b1539b71e2d2030286f8b45f01190adcedc1
Asionsolver <asionsolver274@gmail.com>
2022-06-14 10:13:32 +0600

    modified Labpratice-1 file

--- Lab_Practice_01.sql
+++ Lab_Practice_01.sql
@@ -75,24 +75,31 @@ select Customer_Name,Customer_Country FROM customer ORDER BY Customer_Country DE
 
 /* Display the number of rows in customer table. */
 
-SELECT COUNT(*) as Number_of_Rows
-FROM customer;
+select COUNT(*) as Number_of_Rows
+from customer;
 
 /* Display the number of NULL values in customer table. */
+alter table Customer add Amount integer NULL;
+
+select count(*) as Null_Value from Customer where Amount IS NULL;
+
+/* Display the highest, lowest, and average salary in loan table */
+
+select MAX(Amount) 'Maximum', MIN(Amount) 'Minimum', SUM(Amount) 'Sum', AVG(Amount) 'Average' from loan;
 
 
+/* Display the lowest customer name from both customer and loan table alphabetically.  */
 
 
+/*  Show the student name and CGPA of those students where CGPA between 3.50-3.99 */
 
+select *  
+from Student  
+where CGPA between 3.5 and 3.99; 
 
-
-
-
-
-
-
-
-
+/* Delete the data of those students where the CGPA below 3.50 using sub query. */
+delete from student where CGPA < 3.5;
+  
 
 
 
